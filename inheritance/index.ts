b2b6aff5ad8aed250ea2487bc892660ed1889385
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-08-03 02:56:11 +0300

    inheritance

--- inheritance/index.ts
+++ inheritance/index.ts
@@ -1,16 +1,61 @@
-class Animal {
-	constructor(public name: string) {}
+class Product {
+	constructor(
+		public id: string,
+		public price: number,
+		public description: string,
+	) {}
 
-	move(distance: number): void {
-		console.log(`${this.name} moved ${distance} meters`);
+	display(): void {
+		console.log(`
+		id:${this.id},
+		price: ${this.price},
+		desc: ${this.description}
+		`);
 	}
 }
 
-class Dog extends Animal {
-	constructor(public name: string = 'dog') {
-		super(name);
+class Book extends Product {
+	constructor(
+		public id: string,
+		public price: number,
+		public description: string,
+		public author: string,
+		public title: string,
+	) {
+		super(id, price, description);
+	}
+
+	display() {
+		super.display();
+		console.log(`
+		author: ${this.author},
+		title: ${this.title}
+		`);
 	}
 }
 
-const dog = new Dog('max');
-dog.move(300);
+class Electronic extends Product {
+	constructor(
+		public id: string,
+		public price: number,
+		public description: string,
+		public brand: string,
+		public model: string,
+	) {
+		super(id, price, description);
+	}
+
+	display(): void {
+		super.display();
+		console.log(`
+		brand: ${this.brand},
+		model: ${this.model}
+		`);
+	}
+}
+
+const book = new Book('1', 200, 'the best book', 'aldod', 'the dod');
+book.display();
+
+const electronic = new Electronic('2', 2000, 'the best book', 'HP', 'zedbook');
+electronic.display();
